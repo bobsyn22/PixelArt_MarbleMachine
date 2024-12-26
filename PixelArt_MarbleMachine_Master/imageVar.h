ce4352b367d6fffa9654d0ddd2b6d46a658c6ef6
Bobsy Narayan <83941340+bobsyn22@users.noreply.github.com>
2024-12-25 18:59:34 -0800

    Add files via upload

---
+++ PixelArt_MarbleMachine_Master/imageVar.h
@@ -0,0 +1,10 @@
+#ifndef IMAGEVAR_H
+#define IMAGEVAR_H
+
+// Define the size of the image
+#define IMAGE_SIZE 50
+
+// Declare the external array
+extern const char* const imageArray[IMAGE_SIZE][IMAGE_SIZE];
+
+#endif // IMAGEVAR_H
